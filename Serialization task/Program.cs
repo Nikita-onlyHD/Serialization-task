b0fffaa490a85ee3548f10a91d3581910fb76591
Nikita-onlyHD <DarkDantari@gmail.com>
2023-07-11 21:18:29 +0300

    Re

--- Serialization task/Program.cs
+++ Serialization task/Program.cs
@@ -1,82 +1,136 @@
 ﻿using System;
+using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Serialization_task
 {
+    #region Models
+    class Person
+    {
+        public Int32 Id { get; set; }
+        public Guid TransportId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public Int32 SequenceId { get; set; }
+        public String[] CreditCardNumbers { get; set; }
+        public Int32 Age { get; set; }
+        public String[] Phones { get; set; }
+        public Int64 BirthDate { get; set; }
+        public Double Salary { get; set; }
+        public Boolean IsMarred { get; set; }
+        public Gender Gender { get; set; }
+        public Child[] Children { get; set; }
+    }
+    class Child
+    {
+        public Int32 Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public Int64 BirthDate { get; set; }
+        public Gender Gender { get; set; }
+    }
+    enum Gender
+    {
+        Male,
+        Female
+    }
+    #endregion
+
     class Program
     {
-        static void Main(string[] args)
+        private static readonly Random rnd = new();
+
+        private static Person GenerateRandomPerson(int i)
         {
-            // Generation of 10000 random objects
+            int month = rnd.Next(1, 12);
+            int day = rnd.Next(1, 28);
+            int year = rnd.Next(1960, 2000);
+            var birthDate = new DateTime(year, month, day);
+            var age = DateTime.UtcNow.Year - birthDate.Year;
+            var childBirthDate = new DateTime(year + rnd.Next(18, 23), month, day);
+
+            var person = new Person()
+            {
+                Id = -1,
+                TransportId = Guid.NewGuid(),
+                FirstName = $"FirstName{i}",
+                LastName = $"LastName{i}",
+                SequenceId = -1,
+                CreditCardNumbers = new[] { $"{i}", $"{i + 1}" },
+                Age = age,
+                Phones = new[] { $"+7{i}" },
+                BirthDate = ((DateTimeOffset)birthDate).ToUnixTimeSeconds(),
+                Salary = (double)rnd.Next(20000, 90000),
+                IsMarred = 1 == rnd.Next(2),
+                Gender = rnd.Next(2) == 1 ? Gender.Male : Gender.Female,
+                Children = new[]
+                {
+                    new Child()
+                    {
+                        Id = 0,
+                        FirstName = $"ChildFirstName{i}",
+                        LastName = $"ChildLastName{i}",
+                        BirthDate = ((DateTimeOffset)childBirthDate).ToUnixTimeSeconds(),
+                        Gender = rnd.Next(2) == 1 ? Gender.Male : Gender.Female
+                    }
+                }
+            };
+
+            return person;
+        }
+
+        private static List<Person> GenerateListRandomPersons(int count)
+        {
             List<Person> persons = new List<Person>();
 
-            var rnd = new Random();
-
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < count; i++)
             {
-                int month = rnd.Next(1, 12);
-                int day = rnd.Next(1, 28);
-                int year = rnd.Next(1960, 2000);
-                var birthDate = new DateTime(year, month, day);
-                var age = DateTime.UtcNow.Year - birthDate.Year;
-                var childBirthDate = new DateTime(year + rnd.Next(18, 23), month, day);
-
-
-                var person = new Person()
-                {
-                    Id = i,
-                    TransportId = Guid.NewGuid(),
-                    FirstName = $"FirstName{i}",
-                    LastName = $"LastName{i}",
-                    SequenceId = i,
-                    CreditCardNumbers = new[] { $"{i}", $"{i + 1}" },
-                    Age = age,
-                    Phones = new[] { $"+7{i}" },
-                    BirthDate = ((DateTimeOffset)birthDate).ToUnixTimeSeconds(),
-                    Salary = (double)rnd.Next(20000, 90000),
-                    IsMarred = 1 == rnd.Next(2),
-                    Gender = rnd.Next(2) == 1 ? Gender.Male : Gender.Female,
-                    Children = new[]
-                    {
-                        new Child()
-                        {
-                            Id = 0,
-                            FirstName = $"ChildFirstName{i}",
-                            LastName = $"ChildLastName{i}",
-                            BirthDate = ((DateTimeOffset)childBirthDate).ToUnixTimeSeconds(),
-                            Gender = rnd.Next(2) == 1 ? Gender.Male : Gender.Female
-                        }
-                    }
-                };
+                var person = GenerateRandomPerson(i);
 
                 persons.Add(person);
             }
 
+            return persons;
+        }
+
+        static void Main(string[] args)
+        {
             // Directory selection by user
-            Console.WriteLine("Enter the path: ");
-            string path = Console.ReadLine();
-            var fileName = $"{path}/Persons.json";
+            string path;
+
+            while (true)
+            {
+                Console.Write("Enter the path: ");
+                path = Console.ReadLine()!;
+                
+
+                if (Directory.Exists(path))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid path");
+            }
+
+            var fileName = $"{path}\\Persons.json";
+
+            // Generation of 10000 random objects
+            var persons = GenerateListRandomPersons(10000);
 
             // Convert list to JSON and create Persons.json
             var serializeOptions = new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 WriteIndented = true,
-                MaxDepth = 5
+                MaxDepth = 4
             };
 
             var jsonString = JsonSerializer.Serialize(persons, serializeOptions);
 
-            try
-            {
-                File.WriteAllText(fileName, jsonString);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Wrong path");
-            }
+            File.WriteAllText(fileName, jsonString);
             
+
             // Clear memory
             persons.Clear();
 
@@ -111,38 +165,4 @@ namespace Serialization_task
             Console.WriteLine($"Average value of child age: {ageSum / childCount}");
         }
     }
-    
-
-    #region Models
-    class Person
-    {
-        public Int32 Id { get; set; }
-        public Guid TransportId { get; set; }
-        public String FirstName { get; set; }
-        public String LastName { get; set; }
-        public Int32 SequenceId { get; set; }
-        public String[] CreditCardNumbers { get; set; }
-        public Int32 Age { get; set; }
-        public String[] Phones { get; set; }
-        public Int64 BirthDate { get; set; }
-        public Double Salary { get; set; }
-        public Boolean IsMarred { get; set; }
-        public Gender Gender { get; set; }
-        public Child[] Children { get; set; }
-    }
-    class Child
-    {
-        public Int32 Id { get; set; }
-        public String FirstName { get; set; }
-        public String LastName { get; set; }
-        public Int64 BirthDate { get; set; }
-        public Gender Gender { get; set; }
-    }
-    enum Gender
-    {
-        Male,
-        Female
-    }
-
-    #endregion
 }
